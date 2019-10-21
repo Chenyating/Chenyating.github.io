f41ae4f24601cef16efd5e01d1591b0ad507a585
Cyating <924471303@qq.com>
2019-10-21 09:48:08 +0800

    try against

--- deploy.sh
+++ deploy.sh
@@ -9,17 +9,11 @@ npm run docs:build
 # 进入生成的文件夹
 cd docs/.vuepress/dist
 
-# 如果是发布到自定义域名
-# echo 'www.example.com' > CNAME
-
 git init
 git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
 git push -f git@github.com:Chenyating/Chenyating.github.io.git master
-
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
 cd -
