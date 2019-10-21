d0ef419cc4ddf4558fe3ee0bd0349d58665362c4
Cyating <924471303@qq.com>
2019-10-21 10:01:10 +0800

    try

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,9 @@ git init
 git add -A
 git commit -m 'deploy'
 
+git config user.name "Chenyating"
+git config user.email "924471303@qq.com"
+
 # 如果发布到 https://<USERNAME>.github.io
 git push -f git@github.com:Chenyating/Chenyating.github.io.git master
 
